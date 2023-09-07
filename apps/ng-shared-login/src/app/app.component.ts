c18ebf9e24098f7c82868ce4b47e7c4a778aabaa
Christopher Hair <mystikweb@live.ca>
2023-09-07 11:51:27 -0400

    Initial commit

---
+++ apps/ng-shared-login/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-shared-login-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ng-shared-login';
+}
